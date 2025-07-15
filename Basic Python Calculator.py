8a790a057549b538597a675951a17d6f8ce252c6
manav000091 <168769163+manav000091@users.noreply.github.com>
2025-07-16 01:23:02 +0530

    Add files via upload

---
+++ Basic Python Calculator.py
@@ -0,0 +1,23 @@
+print("Welcome to my basic calculator")
+
+# Take input from user
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Perform calculations
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Cannot divide by zero"
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
